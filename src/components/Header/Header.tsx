ac7f907087694ddea2bd4f5b2d2dfd6d3a0902e1
Sem1337 <semenvlasov2000@gmail.com>
2024-06-23 20:21:18 +0700

    fixup

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,7 +2,7 @@ import {TonConnectButton} from "@tonconnect/ui-react";
 
 export const Header = () => {
     return (
-        <header style={{display: 'flex', alignItems: 'center', justifyContent: 'space-between', marginBottom: '20px'}}>
+        <header>
             <TonConnectButton />
         </header>
     );
