0a08d4c0f0a3a86c74a12f2e4d4870501299ae66
Sem1337 <semenvlasov2000@gmail.com>
2024-06-23 18:54:34 +0700

    wallet and address components

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import WebApp from '@twa-dev/sdk'
 import { Header } from './components/Header/Header'
+import { AddressInfo } from './components/AddressInfo/AddressInfo'
+import { WalletInfo } from './components/WalletInfo/WalletInfo'
 
 /*
 export const Address = () => {
@@ -36,30 +38,11 @@ export const Wallet = () => {
 
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
     <>
     <Header />
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-        {/* Here we add our button with alert callback */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
+    <AddressInfo />
+    <WalletInfo />
     </>
   )
 }
