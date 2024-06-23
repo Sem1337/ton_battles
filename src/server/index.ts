a6fd5ca5968e015f14fc7f9da7baf0b08b711c9c
Sem1337 <semenvlasov2000@gmail.com>
2024-06-24 01:26:14 +0700

    fixup

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 13337;
 
 
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (_: Request, res: Response) => {
     res.send('Hello, TypeScript Express!');
 });
 
