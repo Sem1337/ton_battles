8ac080556a1851e903bc157dad2f300346f2b558
Sem1337 <semenvlasov2000@gmail.com>
2024-06-23 15:19:01 +0700

    wallet address

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,41 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WebApp from '@twa-dev/sdk'
-import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
+import { TonConnectUIProvider, TonConnectButton, useTonAddress, useTonWallet } from '@tonconnect/ui-react';
+
+export const Address = () => {
+  const userFriendlyAddress = useTonAddress();
+  const rawAddress = useTonAddress(false);
+
+  return (
+    userFriendlyAddress && (
+          <div>
+              <span>User-friendly address: {userFriendlyAddress}</span>
+              <span>Raw address: {rawAddress}</span>
+          </div>
+      )
+  );
+};
+
+export const Wallet = () => {
+  const wallet = useTonWallet();
+
+  return (
+      wallet && (
+          <div>
+              <span>Connected wallet: {wallet.account.address}</span>
+              <span>Device: {wallet.device.appName}</span>
+          </div>
+      )
+  );
+};
 
 export const Header = () => {
   return (
       <header>
-          <span>My App with React UI</span>
           <TonConnectButton />
+          <Address />
+          <Wallet />
       </header>
   );
 };
@@ -17,30 +45,37 @@ export const Header = () => {
 function App() {
   const [count, setCount] = useState(0)
   return (
-    <TonConnectUIProvider manifestUrl="https://sem1337.github.io/tonconnect-manifest.json">
-    <>
-    <Header></Header>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-        {/* Here we add our button with alert callback */}
-      <div className="card">
-        <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
-            Show Alert
-        </button>
-      </div>
-    </>
+    <TonConnectUIProvider 
+      manifestUrl="https://sem1337.github.io/tonconnect-manifest.json"
+      actionsConfiguration={{
+        twaReturnUrl: 'https://t.me/ton_battles_bot'
+      }}
+    > 
+    {
+      <>
+      <Header />
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+        </div>
+          {/* Here we add our button with alert callback */}
+        <div className="card">
+          <button onClick={() => WebApp.showAlert(`Hello World! Current count is ${count}`)}>
+              Show Alert
+          </button>
+        </div>
+      </>
+    }
     </TonConnectUIProvider>
   )
 }
