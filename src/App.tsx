e077a9693f435b8de791cb2c1cdcf58cc696a571
Sem1337 <semenvlasov2000@gmail.com>
2024-06-23 15:00:46 +0700

    fixup

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,21 @@ import './App.css'
 import WebApp from '@twa-dev/sdk'
 import { TonConnectUIProvider, TonConnectButton } from '@tonconnect/ui-react';
 
+export const Header = () => {
+  return (
+      <header>
+          <span>My App with React UI</span>
+          <TonConnectButton />
+      </header>
+  );
+};
+
 function App() {
   const [count, setCount] = useState(0)
   return (
     <TonConnectUIProvider manifestUrl="https://sem1337.github.io/tonconnect-manifest.json">
     <>
+    <Header></Header>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -35,13 +45,6 @@ function App() {
   )
 }
 
-export const Header = () => {
-  return (
-      <header>
-          <span>My App with React UI</span>
-          <TonConnectButton />
-      </header>
-  );
-};
+
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App, { Header } from './App.tsx'
 import './index.css'
 import WebApp from '@twa-dev/sdk'
 
