f8911a87f6be8eb0635505b411320e837c7925e7
Sem1337 <semenvlasov2000@gmail.com>
2024-07-01 19:02:22 +0700

    fixup

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,14 +1,11 @@
 import { Sequelize } from 'sequelize';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USERNAME as string,
   process.env.DB_PASSWORD as string,
   {
-    host: process.env.DB_HOST,
+    host: '127.0.0.1',
     dialect: 'postgres',
     port: parseInt(process.env.DB_PORT as string, 10),
     logging: false, // Disable logging (optional)
