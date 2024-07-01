1ede14c481f3de997ea03770c77d0e094caab98f
Sem1337 <semenvlasov2000@gmail.com>
2024-07-01 17:59:46 +0700

    fixup

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,21 +7,21 @@ export default {
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'testdb',
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: '127.0.0.1',
     dialect: 'postgres'
   },
   test: {
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'testdb',
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: '127.0.0.1',
     dialect: 'postgres'
   },
   production: {
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'testdb',
-    host: process.env.DB_HOST || '127.0.0.1',
+    host: '127.0.0.1',
     dialect: 'postgres'
   }
 };
