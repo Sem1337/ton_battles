960c9b3f75ee5ba955eda0971c02c8a5057b5bd2
Sem1337 <semenvlasov2000@gmail.com>
2024-07-01 19:55:40 +0700

    logs

--- backend/src/database/db.ts
+++ backend/src/database/db.ts
@@ -1,11 +1,19 @@
 import { Sequelize } from 'sequelize';
 
+
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_PORT:', parseInt(process.env.DB_PORT as string, 10));
+
+
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USERNAME as string,
   process.env.DB_PASSWORD as string,
   {
-    host: '127.0.0.1',
+    host: process.env.DB_HOST,
     dialect: 'postgres',
     port: parseInt(process.env.DB_PORT as string, 10),
     logging: false, // Disable logging (optional)
