58a8b6cc3858cebe12020116504bda6b7d4218dc
Sem1337 <semenvlasov2000@gmail.com>
2024-07-04 22:06:09 +0700

    fixup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,8 @@ const port = process.env.PORT || 8080;
 
 app.use(cors({
     origin: 'https://sem1337.github.io', // Replace with your frontend URL
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
 }));
 
 app.use(bodyParser.json());
