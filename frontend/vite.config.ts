0c15af80875476273307c99e6adb1699915fa424
Sem1337 <semenvlasov2000@gmail.com>
2024-07-09 04:07:13 +0700

    fixup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nodePolyfills(), react()],
+  plugins: [react(), nodePolyfills()],
   server: {
     https: {
       key: '../certs/localhost+2-key.pem',
