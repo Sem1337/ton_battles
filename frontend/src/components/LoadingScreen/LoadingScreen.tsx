2840cb196ebbe73c278c44e3b8babc433535c2ce
Sem1337 <semenvlasov2000@gmail.com>
2024-07-18 02:49:44 +0700

    fixup

--- frontend/src/components/LoadingScreen/LoadingScreen.tsx
+++ frontend/src/components/LoadingScreen/LoadingScreen.tsx
@@ -2,11 +2,8 @@ import React from 'react';
 
 const LoadingScreen: React.FC = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-cover bg-center" style={{ backgroundImage: `url(./test.png)` }}>
-      <div className="text-center">
-        <div className="loader mx-auto mb-4"></div>
-        <p className="text-white text-2xl font-bold">Loading...</p>
-      </div>
+    <div className="loading-screen">
+      <div className="spinner"></div>
     </div>
   );
 };
