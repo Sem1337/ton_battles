b0f33e2caa0fea8fdc0bff5066b663e65330ac68
Sem1337 <semenvlasov2000@gmail.com>
2024-07-25 18:56:29 +0700

    fixup

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import { useSocket } from '../../contexts/SocketContext';
-import PointsCounter from '../PointsCounter/PointsCounter'
+import PointsCounter from '../PointsCounter/PointsCounter';
 import './Home.css';
 
 const Home: React.FC = () => {
   const { sendMessage } = useSocket();
   useEffect(() => {
-    console.log('balance info use effect')
+    console.log('balance info use effect');
     sendMessage('GET_BALANCE');
   }, []);
 
@@ -29,7 +29,7 @@ const Home: React.FC = () => {
       <Link to="/shop" className="link-button">
         Shop
       </Link>
-      <Link to="/referral" className="link-button">
+      <Link to="/referral" className="link-button referral">
         Referral Page
       </Link>
     </div>
