be7b6426e6e82d7c96281264a56b3f13ba50d837
Sem1337 <semenvlasov2000@gmail.com>
2024-07-31 06:06:38 +0700

    fixup

--- backend/config/config.js
+++ backend/config/config.js
@@ -8,20 +8,38 @@ export default {
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'testdb',
     host: process.env.DB_HOST || 'test',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: 6432,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, '/root/.postgresql/root.crt')).toString(),
+      },
+    },
   },
   test: {
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'testdb',
     host: process.env.DB_HOST || 'test',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: 6432,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, '/root/.postgresql/root.crt')).toString(),
+      },
+    },
   },
   production: {
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_NAME || 'testdb',
     host: process.env.DB_HOST || 'test',
-    dialect: 'postgres'
+    dialect: 'postgres',
+    port: 6432,
+    dialectOptions: {
+      ssl: {
+        ca: fs.readFileSync(path.resolve(__dirname, '/root/.postgresql/root.crt')).toString(),
+      },
+    },
   }
 };
